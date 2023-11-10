0ad6058dc027fe4a57074f1577bb67a1b09b0280
Yun-Rou <dackdada016@gmail.com>
2023-11-10 15:48:04 +0800

    fix : 修正無法正確引入圖片路徑

--- src/data/LatesEventsData.js
+++ src/data/LatesEventsData.js
@@ -1,20 +1,27 @@
-export const events = [
+import Cover from '../assets/images/cover.png'
+import CoverCat from '../assets/images/covercat.png'
+import CoverCatFirst from '../assets/images/covercatFirst.png'
+
+export const LatesEventsData = [
   {
     id: 0,
     title: "參與台北寵物論壇，爭取貓咪友善環境",
     description: "炎炎夏日的周六，我走進了台北寵物論壇，帶著一副貓耳髮箍，決定要全力宣傳「貓咪至上」的理念！我相信，我們的都市中，每一隻貓咪都應該有自己的 VIP 休憩空間。",
     date: "2023/12/26",
+    imgSrc: `${CoverCatFirst}`
   },
   {
     id: 1,
     title: "掃街模式開啟！帶著你的貓耳，來和我一起走！",
     description: "街上氣氛真的很棒，從小孩到大人，甚至有些狗狗朋友都帶著貓耳來找我握手，真的太可愛了！",
-    date: "2023/12/24"
+    date: "2023/12/24",
+    imgSrc: `${Cover}`
   },
   {
     id: 2,
     title: "收容所模特兒大比拼！",
     description: "今天的收容所不再是一片寂靜。為了讓更多人認識到這裡的毛孩子，我們舉辦了一場前所未有的「模特兒走秀」！",
-    date: "2023/12/20"
+    date: "2023/12/20",
+    imgSrc:`${CoverCat}`
   },
 ];
