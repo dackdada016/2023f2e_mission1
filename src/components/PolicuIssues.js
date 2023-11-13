e7ac049016d0486014c251a1deb75b165d801a66
Yun-Rou <dackdada016@gmail.com>
2023-11-13 10:45:27 +0800

    feat : 新增元件區塊

--- src/components/PolicuIssues.js
+++ src/components/PolicuIssues.js
@@ -1,23 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
+import { PolicuIssuesData } from '../data/PolicuIssiesData.js'
+
+const PolicuIssuesTitle = styled.div`
+  display:flex;
+  flex-direction:column;
+  align-items: center;
+  margin:auto;
+  span{
+    font-weight: 600;
+    padding: 0.5rem 0.75rem;
+    border-radius: 0.5rem;
+    background: rgba(51, 65, 85, 1);
+    color:rgba(255, 255, 255, 1);
+  }
+  h2{
+    margin: 0;
+    font:400 4rem 'Mantou Sans';
+    background: -webkit-linear-gradient(45deg, #E6793B 1.54%, #FF4185 97.86%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    color: transparent;
+    line-height: 93px;
+  }
+
+`
+const IssuesCard = PolicuIssuesData.map((item)=>(
+  <div key={item.id}>
+    <div>
+      <h4>{item.cardTitle}</h4>
+    </div>
+    <img src={item.imgSrc} alt={item.description} />
+  </div>
+));
+
 
 function PolicuIssues() {
   return (
     <section class="policu-issues">
-      <div class="policu-issues-title">
-        <div class="policu-issues-label">policu issues</div>
-        <h1>政策議題</h1>
-      </div>
-      <div class="issues-card">
-        <div class="card-title"></div>
-        <img src="" alt="" />
-      </div>
-      <div class="issues-card">
-        <div class="card-title"></div>
-        <img src="" alt="" />
-      </div>
-      <div class="issues-card">
-        <div class="card-title"></div>
-        <img src="" alt="" />
+      <PolicuIssuesTitle>
+        <span>policu issues</span>
+        <h2>政策議題</h2>
+      </PolicuIssuesTitle>
+      <div>
+        {IssuesCard}
       </div>
     </section>
   )
---
+++ src/data/PolicuIssiesData.js
@@ -0,0 +1,24 @@
+import CardImage1 from '../assets/images/保障專案.png'
+import CardImage2 from '../assets/images/休閒天堂.png'
+import CardImage3 from '../assets/images/推廣寵物飼養教育.png'
+
+export const PolicuIssuesData = [
+  {
+    id: 0,
+    cardTitle:"為毛孩子謀福利！推動寵物醫療保障方案",
+    description:"推動寵物醫療保障方案",
+    imgSrc:`${CardImage1}`
+  },
+  {
+    id: 1,
+    cardTitle:"打造休閒天堂！推廣寵物休閒與娛樂場所",
+    description:"推廣寵物休閒與娛樂場所",
+    imgSrc:`${CardImage2}`
+  },
+  {
+    id: 2,
+    cardTitle:"推廣寵物飼養教育，讓愛更加專業",
+    description:"推廣寵物飼養教育",
+    imgSrc:`${CardImage3}`
+  }
+]
