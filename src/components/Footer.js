fa974e17e9085a7b0d401fb2718b5e84c4a6b028
Yun-Rou <dackdada016@gmail.com>
2023-11-15 17:54:26 +0800

    feat : 新增icon元件

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import IconGrouo from './IconGrouo'
+import styled from 'styled-components'
+
+const FooterContainer = styled.footer`
+  background: var(--bg-2, #F7ECE1);
+`
 
 function Footer() {
   return (
-    <footer>
+    <FooterContainer>
       <article>
         <p>喵立翰 Mioa Li-Han</p>
-        <div class="icon-group"></div>
+        <IconGrouo />
         <p>&copy; 2024 喵立翰 Mioa Li-Han 版權所有</p>
       </article>
       <address>
@@ -16,7 +22,7 @@ function Footer() {
           <li></li>
         </ul>
       </address>
-    </footer>
+    </FooterContainer>
   )
 }
 
