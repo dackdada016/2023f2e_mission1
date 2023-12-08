47cc87f39e512b79a653b3fa36d779e0fd7ef272
Yun-Rou <dackdada016@gmail.com>
2023-12-08 15:13:21 +0800

    chore : 新增註解

--- src/components/contexts/ModalProvider.js
+++ src/components/contexts/ModalProvider.js
@@ -1,11 +1,12 @@
 import React, { createContext, useState, useContext, useEffect} from 'react'
-
+// 建立狀態容器
 const ModalContext = createContext();
-
+// 建立跨元件調用屬性的hook
 export const useModal = () => useContext(ModalContext);
-
+// 封裝負責管理modal的開啟與關閉邏輯
 export const  ModalProvider = ({ children }) => {
-  // const [ activeModal, setActiveModal ] = useState(null);
+
+  // 渲染邏輯
   const [ modalConfig, setModalConfig ] = useState({ component: null, props:{} }) 
   const openModal = (config) => {
     setModalConfig(config)
@@ -14,28 +15,28 @@ export const  ModalProvider = ({ children }) => {
     setModalConfig({ component: null, props:{} })
   };
 
-  
+  // 判斷是否觸發屬性變更來重新註冊事件監聽
   useEffect(() => {
     const handleKeyDown = (event) => {
-      if (event.keyCode === 27) { // 27 是 ESC 鍵的鍵碼
+      if (event.keyCode === 27) {
         closeModal();
       }
     };
 
-    // 添加鍵盤事件監聽器
+    // 加入事件監聽器
     window.addEventListener('keydown', handleKeyDown);
 
-    // 清理函數
+    // 移除事件監聽器
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
   }, [closeModal]);
-
+  // 解構屬性值方便調用程式碼
   const { component: ModalComponent, props: modalProps } = modalConfig
   return(
     <ModalContext.Provider value={{ modalConfig, openModal, closeModal}} >
       {children}
-    {ModalComponent && <ModalComponent {...modalProps} />}
+      {ModalComponent && <ModalComponent {...modalProps} />}
     </ModalContext.Provider>
   )
 };
